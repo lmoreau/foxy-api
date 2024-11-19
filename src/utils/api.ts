b0df4e445ec692866c9f590d35dd7875ffd146c3
Lee Moreau <lmoreau@infusion-it.com>
2024-11-19 12:31:21 -0500

    Remove unused listMasterResidualRows function and Azure Function

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -171,13 +171,6 @@ export const listQuoteLineItemByRow = async (locationId: string) => {
   return response.data;
 };
 
-export const listMasterResidualRows = async () => {
-  const headers = await getAuthHeaders();
-  const url = `${API_BASE_URL}/listMasterResidualRows`;
-  const response = await axios.get(url, { headers });
-  return response.data.value;
-};
-
 export const listRogersWirelineRecords = async (accountId: string) => {
   const headers = await getAuthHeaders();
   const url = `${API_BASE_URL}/listRogersWirelineRecords?accountId=${accountId}`;
