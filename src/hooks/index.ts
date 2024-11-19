beb7c50d4cb737309cc2ade854026783e8452277
Lee Moreau <lmoreau@infusion-it.com>
2024-11-19 11:35:09 -0500

    Remove useResidualDetailsData export from hooks index

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,5 +2,4 @@ export * from './useTimelineData';
 export * from './useQuoteData';
 export * from './useModal';
 export * from './useQuoteLineItems';
-export * from './useResidualDetailsData';
 export * from './useServiceWirelinePayments';
