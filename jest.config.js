1bb527f761ab70c5790c5eac41af3d602fdfd429
Lee Moreau <lmoreau@infusion-it.com>
2024-11-19 16:08:28 -0500

    Fixing workflow

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  transformIgnorePatterns: [
+    "node_modules/(?!(axios)/)"
+  ],
+  transform: {
+    '^.+\\.(ts|tsx|js|jsx)$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+};
